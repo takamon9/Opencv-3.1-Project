6ddd53a9f8c497c0c25e85a5209c5db6295b8d39
Yukihiro Takamoto <organowhatthehell1974@gmail.com>
2016-10-23 09:32:25 +0900

    Update Create "info.dat" file.cpp

--- Create "info.dat" file.cpp
+++ Create "info.dat" file.cpp
@@ -7,83 +7,78 @@
 #include <opencv2\opencv.hpp>
 #include <string>
 #include <fstream>
+#include <sstream>
 
 using namespace std;
 using namespace cv;
 
 // You have to install OpenCv 2.4.x or later version 3.x to compile this code. 
+// Also,info.dat shoud be saved by ANSI Code,not by unicode or UTF-8.
 
 int main(void) {
 
-	string posFileName[500];
-	Mat posSrcImage[500];
-	Mat positiveImage[500];
-	string width,height;
-	ofstream ofs("C:/cascade//pos//info.dat");
+	string posFileName[1500];
+	Mat posSrcImage[1500];
+	Mat positiveImage[1500];
+	string width, height;
 
-	for (int i = 1; i < 50; i++){
+	string fnum = "num_4"; //Change this num "num_x"
+	int fileNumber = 234; //Input File Number
+
+	ofstream ofs("C:/numSets/" + fnum + "/pos/info.dat");
+	int a = 1;
+
+	for (int i = 1; i < fileNumber + 1 ; i++) {  // input number of image files + 1
+
+		stringstream ssname;
+		stringstream ssrenamed;
+
+		ssname << "C:/numSets/" + fnum + "/pos/num (" << a << ").png";
+
+		string name = ssname.str();
+		const char *cname = name.c_str();
+		cout << cname << endl;
+
+		ssrenamed << "C:/numSets/" + fnum + "/pos/" << a << ".png";
+
+		string name2 = ssrenamed.str();
+		const char *namerename = name2.c_str();
+		cout << " to " << namerename << endl;
+
+		rename(cname, namerename);
+
+		a++;
 
 		posFileName[i] = to_string(i) + ".png";
-
-		posSrcImage[i] = imread("C:/cascade//pos//" + posFileName[i]);
+		posSrcImage[i] = imread("C:/numSets/" + fnum + "/pos/" + posFileName[i]);
 		width = to_string(posSrcImage[i].size().width);
 		height = to_string(posSrcImage[i].size().height);
 
-		ofs << posFileName[i] + " 1 0 0 " + width + " " + height +  "\n";
+		ofs << posFileName[i] + " 1 0 0 " + width + " " + height + "\n";
 
+		cout << "info.dat file correctly created! Press Any Key to finish." << endl;
 	}
-
-	/*
-		HANDLE hSearch, hFile;
-		WIN32_FIND_DATA fd;
-		DWORD writeSize;
-		TCHAR filename[MAX_PATH + 10];
-
-
-		// Create new file
-		hFile = CreateFile(
-			_T("C:/cascade//pos//info.dat"), GENERIC_WRITE, 0, NULL,
-			CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL
-			);
-		if (hFile == INVALID_HANDLE_VALUE)
-		{
-			return 0;
-		}
-
-		// Find all files
-		hSearch = FindFirstFile(_T("C:/cascade//pos//*.png"), &fd);
-		if (hSearch == INVALID_HANDLE_VALUE)
-		{
-			return 0;
-		}
-
-		while (TRUE)
-		{
-
-			// Writting down files finded.
-			_tcscpy(filename, fd.cFileName); // Copy file names
-			_tcscat(filename, _T(" 1 0 0 \r\n"));   // Input end line.
-			WriteFile(
-				hFile, filename, (DWORD)(_tcslen(filename) * sizeof(TCHAR)),
-				&writeSize, NULL
-				);
-
-			if (!FindNextFile(hSearch, &fd))
-
-			{
-				if (GetLastError() == ERROR_NO_MORE_FILES)
-					break;
-				else
-					break;
-			}
-		}
-
-		FindClose(hSearch);
-		CloseHandle(hFile);
-
-		*/
-	cout << "info.dat file correctly created! Press Any Key to finish."<< endl;
-
 	system("pause");
 	return 0;
 }
+
+/*	void renameFunction (){
+
+		string  str1("C:/numSets/num_0/pos/num0\(" + to_string(i) + ").png");
+		const char *ptr1 = 0;
+		ptr1 = str1.data();
+		const char *c_str1 = str1.c_str();
+
+		string  str2("C:/numSets/num_0/pos/" + to_string(i) + ".png");
+		const char *ptr2 = 0;
+		ptr2 = str2.data();
+		const char *c_str2 = str2.c_str();
+
+		cout << str1 << "==>"  << str2 << endl;
+		//	cout << c_str1 << "\n" <<  c_str2 << endl;
+		rename(c_str1, c_str2);
+
+		//	delete(ptr1);
+		//	delete(ptr2);
+	
+	}*/
