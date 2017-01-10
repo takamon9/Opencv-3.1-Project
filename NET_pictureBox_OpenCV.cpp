df8ca493b375edfe97209bacb161c41aa533c171
Yukihiro Takamoto <organowhatthehell1974@gmail.com>
2017-01-10 10:54:26 +0900

    Create NET_pictureBox_OpenCV.cpp

---
+++ NET_pictureBox_OpenCV.cpp
@@ -0,0 +1,20 @@
+
+
+Mat src = Mat(x,y,CV_32S);//
+//OR
+cvtcolor(src,dst,CV_32S);
+
+
+void DrawCVImage(System::Windows::Forms::Control^ control, cv::Mat& colorImage)
+{
+	System::Drawing::Graphics^ graphics = control->CreateGraphics();
+	System::IntPtr ptr(colorImage.ptr());
+	System::Drawing::Bitmap^ b
+		= gcnew System::Drawing::Bitmap(colorImage.cols, colorImage.rows, colorImage.step, System::Drawing::Imaging::PixelFormat::Format24bppRgb, ptr);
+	System::Drawing::RectangleF rect(0, 0, control->Width, control->Height);
+	graphics->DrawImage(b, rect);
+	delete graphics;
+}
+
+
+DrawCVImage(pictureBox1,dst);
